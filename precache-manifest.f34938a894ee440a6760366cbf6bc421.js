5bb0f3014436cc2d8cd9d85a51106eb4913d5ec8
Gillian McDonald <gillymary@beeta.co>
2020-06-30 10:49:01 +0100

    Updates

---
+++ precache-manifest.f34938a894ee440a6760366cbf6bc421.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c1ef191e58a25ecc1ee903df6bf245",
+    "url": "/front-end-evidence/index.html"
+  },
+  {
+    "revision": "6b42c12caf0813e135f5",
+    "url": "/front-end-evidence/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c0b7d3dd9bb559883aee",
+    "url": "/front-end-evidence/static/js/2.aca40f2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front-end-evidence/static/js/2.aca40f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b42c12caf0813e135f5",
+    "url": "/front-end-evidence/static/js/main.0e25054d.chunk.js"
+  },
+  {
+    "revision": "6ac1ec5a67fbe6ed9f90",
+    "url": "/front-end-evidence/static/js/runtime-main.32ef2e0e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front-end-evidence/static/media/logo.5d5d9eef.svg"
+  }
+]);
